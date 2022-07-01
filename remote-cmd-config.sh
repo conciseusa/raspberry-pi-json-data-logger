9381128bdc4ddbeeeb807c5ee642f0434ae1423d
Walter Spurgiasz <walter@conciseusa.com>
2022-06-30 21:54:51 -0700

    Move config out of script

---
+++ remote-cmd-config.sh
@@ -0,0 +1,4 @@
+# create a config dir in the parent dir, copy in this file and set to a file link you control
+# example/testing files
+CMDF="https://conciseusa.github.io/remote-cmd-resetF.json"
+#CMDF="https://conciseusa.github.io/remote-cmd-resetT.json"
