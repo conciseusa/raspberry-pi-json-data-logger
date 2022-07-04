3576232b0b3e470a1639b276f0258f6a4a07d710
conciseusa <walter@conciseusa.com>
2022-07-04 16:14:30 -0700

    Rename reset to reboot to match bash command.

--- remote-cmd-config.sh
+++ remote-cmd-config.sh
@@ -1,4 +1,4 @@
 # create a config dir in the parent dir, copy in this file and set to a file link you control
 # example/testing files
-CMDF="https://conciseusa.github.io/remote-cmd-resetF.json"
-#CMDF="https://conciseusa.github.io/remote-cmd-resetT.json"
+CMDF="https://conciseusa.github.io/remote-cmd-rebootF.json"
+#CMDF="https://conciseusa.github.io/remote-cmd-rebootT.json"
