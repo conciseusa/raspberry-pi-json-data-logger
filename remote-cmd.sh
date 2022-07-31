08d80550e001f91bc77a28bb1706c7864b6dc242
Walter Spurgiasz <walter@conciseusa.com>
2022-07-31 13:36:27 -0700

    Update to work with Python 3, move more config out of script and in to config file.

--- remote-cmd.sh
+++ remote-cmd.sh
@@ -13,36 +13,37 @@ SCRIPT_DIR=$(dirname $(readlink -f "$BASH_SOURCE"))
 # https://stackoverflow.com/questions/59895/how-can-i-get-the-directory-where-a-bash-script-is-located-from-within-the-scrip
 
 # CMDF="http://server.com/cmd.json" # to set the cmd url here, uncomment this line, comment out source command
+# LOG_DIR="/tmp" same with log dir
 # or better to keep config seperate in a config dir in the parent dir
 # source below has worked in testing, but may need to hardcode path in some situations
-source $SCRIPT_DIR/../config/remote-cmd-config.sh
+source $SCRIPT_DIR/../config/cron-config.sh
 echo "$DATE - CMDF = $CMDF"
-echo "$DATE - $SCRIPT_DIR - $0" >> /tmp/remote-cmd.log # for troubleshooting in cron jobs
-echo "$DATE - CMDF = $CMDF" >> /tmp/remote-cmd.log
+echo "$DATE - $SCRIPT_DIR - $0" >> $LOG_DIR/remote-cmd.log # for troubleshooting in cron jobs
+echo "$DATE - CMDF = $CMDF" >> $LOG_DIR/remote-cmd.log
 
 # to remote trigger a git pull, cmd file = {"reboot":"F", "pull":"T", "data":"Test Data"}
 # pull before reboot so updated code will run on reboot
-pull=`curl -s $CMDF | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'pull'"]'`;
+pull=`curl -s $CMDF | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["'pull'"])'`;
 echo "$DATE - pull = $pull"
 if [ $pull = "T" ]; then
-   echo "$DATE - pull" >> /tmp/remote-cmd.log
-   git -C $SCRIPT_DIR pull >> /tmp/remote-cmd.log
+   echo "$DATE - pull" >> $LOG_DIR/remote-cmd.log
+   git -C $SCRIPT_DIR pull >> $LOG_DIR/remote-cmd.log
 fi
 
 # to remote trigger a reboot, cmd file = {"reboot":"T", "pull":"F", "data":"Test Data"}
 # example/testing hosted file with reboot True - https://conciseusa.github.io/remote-cmd-rebootT.json
-reboot=`curl -s $CMDF | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'reboot'"]'`;
+reboot=`curl -s $CMDF | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["'reboot'"])'`;
 echo "$DATE - reboot = $reboot"
 if [ $reboot = "T" ]; then
-   echo "$DATE - reboot" >> /tmp/remote-cmd.log
+   echo "$DATE - reboot" >> $LOG_DIR/remote-cmd.log
    /sbin/reboot
 fi
 
 # to set a file from remote source, cmd file = {"reboot":"F", "pull":"F", "data":"Test Data"}
 # example/testing hosted file with reboot False - https://conciseusa.github.io/remote-cmd-rebootF.json
-if [ 0 = 1 ]; then # disable until needed
-   data=`curl -s $CMDF | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'data'"]'`;
+if [ 1 = 1 ]; then # 0 = 1 disable until needed, 1 = 1 enable
+   data=`curl -s $CMDF | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["'data'"])'`;
    echo "$DATE - data = $data"
-   echo "$DATE - data = $data" >> /tmp/remote-cmd.log
-   echo "$data" > /tmp/remote-cmd.txt
+   echo "$DATE - data = $data" >> $LOG_DIR/remote-cmd.log
+   echo "$data" > $LOG_DIR/remote-cmd.txt # set to file to update
 fi
