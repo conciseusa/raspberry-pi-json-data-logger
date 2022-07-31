89d09cd34cf5bbe42955b4527d5117411a4d9d21
Walter Spurgiasz <walter@conciseusa.com>
2022-07-31 13:33:55 -0700

    Use one config file for crontab scripts.

--- remote-cmd-config.sh
+++
@@ -1,4 +0,0 @@
-# create a config dir in the parent dir, copy in this file and set to a file link you control
-# example/testing files
-CMDF="https://conciseusa.github.io/remote-cmd-rebootF.json"
-#CMDF="https://conciseusa.github.io/remote-cmd-rebootT.json"
