b77b4c10f6fb391c95ed124292bd2287180d401c
Walter Spurgiasz <walter@conciseusa.com>
2022-07-31 13:37:02 -0700

    Use one config file for crontab scripts.

---
+++ cron-config.sh
@@ -0,0 +1,12 @@
+# config for scripts that run from crontab
+# create a config dir in the parent dir, copy in this file
+
+LOG_DIR="/home/pi/log"
+
+# remote-cmd.sh - set to a file link you control
+# example/testing files
+CMDF="https://conciseusa.github.io/remote-cmd-rebootF.json"
+#CMDF="https://conciseusa.github.io/remote-cmd-rebootT.json"
+
+# auto-restart.sh to auto restart logger script at reboot
+# SCRIPT_DIR="/home/pi/Work/git/raspberry-pi-json-data-logger"
